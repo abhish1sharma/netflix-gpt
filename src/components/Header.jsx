da7a269aaffc3dc79e358b516323b2e86cc7db9e
Abhishek Sharma <abhisharmahsr@gmail.com>
2025-09-08 01:24:01 +0530

    unsubscribe the onauthstatechange on unmounting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -30,6 +30,8 @@ const Header = () => {
         navigate("/");
       }
     });
+    // Unsubcribe when component unmounts
+    return () => unsubscribe();
   }, []);
 
   return (
