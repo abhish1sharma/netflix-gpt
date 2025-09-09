e73480b8fc64151f08334b286a0f178497b31e7b
Abhishek Sharma <abhisharmahsr@gmail.com>
2025-09-09 16:48:11 +0530

    autoplay , layering of title and video

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -4,16 +4,14 @@ import useMovieTrailer from "../hooks/useMovieTrailer";
 const VideoBackground = ({ movieId }) => {
   //   const [trailerId, setTrailerId] = useState(null);
   const key = useSelector((store) => store.movie.trailerVideo?.key);
-  
 
   useMovieTrailer(movieId);
 
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${key}`}
+        className="w-screen aspect-video"
+        src={`https://www.youtube.com/embed/${key}?autoplay=1`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-30">
+    <div className="w-screen aspect-video pt-[20%] px-30 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl">{title}</h1>
       <p className=" text-lg py-6 w-1/2">{overview}</p>
       <div>
-        <button className="px-6 py-2 bg-white rounded-xl mx-2 text-2xl text-black">
-         ▶️ Play
+        <button className="px-10 py-2 bg-white rounded-lg mx-2 text-xl text-black hover:opacity-80 cursor-pointer">
+          ▶️ Play
         </button>
-        <button className="px-6 py-2 bg-gray-500 rounded-xl mx-2 text-2xl text-white">
-         ℹ️ More Info
+        <button className="px-10 py-2 bg-gray-500 rounded-lg mx-2 text-xl text-white cursor-pointer">
+          ℹ️ More Info
         </button>
       </div>
     </div>
